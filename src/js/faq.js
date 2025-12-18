5df5ce82c35ac30f350b318eca36e94a133d5227
Nino lefort <115539009+Ninodev14@users.noreply.github.com>
2025-12-18 12:08:48 +0100

    smaler code

--- src/js/faq.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".faq-container-info").forEach(faq => {
-  const header = faq.querySelector(".faq-header");
-  header.addEventListener("click", () => {
-    faq.classList.toggle("active");
-  });
-});
